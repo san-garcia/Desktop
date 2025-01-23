d5ad0e681aafbedbd00eef44cf1bd8b1d96701b8
saangar <sandragarciafabra@gmail.com>
2025-01-23 13:05:22 +0100

    DI y AD

--- DI/2º EVA/Proyecto/proyectoID/src/app/artistas/artistas.component.ts
+++ DI/2º EVA/Proyecto/proyectoID/src/app/artistas/artistas.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+interface Artista {
+  nombre: string;
+  especilidad: string;
+  imagen: string;
+  ig:string
+}
 @Component({
   selector: 'app-artistas',
   imports: [],
@@ -8,5 +13,14 @@ import { Component } from '@angular/core';
 })
 
 export class ArtistasComponent {
-  
+  artistas: Artista[] = [
+    { nombre: 'Jesica', especilidad: 'Especializada en: Tradicional a color/negro y blackwork', imagen: 'artistas/jessica.jpg', ig: 'jesica_36'},
+    { nombre: 'Ana', especilidad: 'Especializada en: Microrealismo, Fineline y Blackwork', imagen: 'artistas/ana.jpg', ig: 'anaplaza.tattoo'},
+    { nombre: 'Joni', especilidad: 'Especializado en: Realismo y microrealismo', imagen: 'artistas/joni.jpg', ig: 'badblood13tattoo'},
+    { nombre: 'Jota', especilidad: 'Especializado en: Blackwork, Dotwork e Ilustración', imagen: 'artistas/jota.jpg', ig: '13tattooer'},
+    { nombre: 'John', especilidad: 'Especializado en: Realismo black and grey y Surrealismo color', imagen: 'artistas/john.jpg', ig: 'jhonbarrios.tattoo'},
+    { nombre: 'Yasmina', especilidad: 'Especializada en: Piercing, microdermal y gemas mentales', imagen: 'artistas/yasmina.jpg', ig: '17.yaspiercing'},
+    { nombre: 'Chaky', especilidad: 'Especializado en: Lettering, Graffiti y Blackwork', imagen: 'artistas/chaky.jpg', ig: 'chakytattoo'},
+    { nombre: 'Jan', especilidad: 'Especializado en: Black n Gray, Geometría y Fine line', imagen: 'artistas/jan.jpg', ig: 'jan_soloink'}
+  ]
 }
