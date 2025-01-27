1f3d501b45f7aec881f1cb02485065fcc98a9c0e
saangar <sandragarciafabra@gmail.com>
2025-01-27 13:54:54 +0100

    DI

--- DI/2º EVA/Proyecto/proyectoID/src/app/services/artistas-service.service.ts
+++ DI/2º EVA/Proyecto/proyectoID/src/app/services/artistas-service.service.ts
@@ -10,7 +10,7 @@ export class ArtistasServiceService {
   // Artistas 
   artistas = [
     { id: '1', nombre: 'Jesica', especilidad: 'Especializada en: Tradicional a color/negro y blackwork', descripcion:'Jessica lleva muchísimo tiempo con nosotros en el estudio. Sus obras han hecho que la gente tenga su tatuaje soñado plasmado en su piel. Los colores que implementa hace que sea toda una obra maestra. Si tienes pensado realizarte un tatuaje con Jessica, ¡no lo dudes!. Te dejamos su contacto, las obras que ha hecho y algunos diseños que tiene disponible.' ,imagen: 'artistas/jessica.jpg', tatuajes: 'tatuajes-jessica', ig: 'jesica_36'},
-    { id: '2', nombre: 'Ana', especilidad: 'Especializada en: Microrealismo, Fineline y Blackwork', imagen: 'artistas/ana.jpg', tatuajes: 'tatuajes-ana',ig: 'anaplaza.tattoo'},
+    { id: '2', nombre: 'Ana', especilidad: 'Especializada en: Microrealismo, Fineline y Blackwork', descripcion: 'Ana es nuestra experta en el microrealismo, fineline y blacwork', imagen: 'artistas/ana.jpg', tatuajes: 'tatuajes-ana',ig: 'anaplaza.tattoo'},
     { id: '3', nombre: 'Joni', especilidad: 'Especializado en: Realismo y microrealismo', imagen: 'artistas/joni.jpg', tatuajes: 'tatuajes-joni',ig: 'badblood13tattoo'},
     { id: '4', nombre: 'Jota', especilidad: 'Especializado en: Blackwork, Dotwork e Ilustración', imagen: 'artistas/jota.jpg', tatuajes: 'tatuajes-jota',ig: '13tattooer'},
     { id: '5', nombre: 'John', especilidad: 'Especializado en: Realismo black and grey y Surrealismo color', imagen: 'artistas/jhon.jpg', tatuajes: 'tatuajes-jhon',ig: 'jhonbarrios.tattoo'},
